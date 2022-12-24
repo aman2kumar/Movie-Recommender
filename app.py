874a0acbac396193bef200d9987acee0323defb0
aman2kumar <amankumar33152@gmail.com>
2022-12-24 16:57:22 +0530

    final version

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Two data: 1) Top 5000 movies  2) more than 45000 movies
+# for 1) movies -> path -> movie_list.pkl
+#        final  -> path -> final.pkl
+#        movie_id = movies.iloc[int(final[index + 1][i])].movie_id
+# for 2) movies -> path -> movies.pkl
+#        final  -> path -> recommended_list.pkl
+#        movie_id = movies.iloc[int(final[index + 1][i])].id
 import pickle
 import streamlit as st
 import requests
@@ -14,12 +21,16 @@ def fetch_poster(movie_id):
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
-
+    self_movie = []
+    self_movie.append(movies.iloc[index].movie_id)
+    self_movie.append(movie)
     # distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in range(5):
+    movie_ids = []
+    for i in range(9):
         movie_id = movies.iloc[int(final[index + 1][i])].movie_id
+        movie_ids.append(movie_id)
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[int(final[index + 1][i])].title)
     # for i in distances[1:6]:
@@ -28,7 +39,7 @@ def recommend(movie):
     #     recommended_movie_posters.append(fetch_poster(movie_id))
     #     recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names,recommended_movie_posters, movie_ids, self_movie
 
 
 st.header('Movie Recommender System')
@@ -43,27 +54,122 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    recommended_movie_names, recommended_movie_posters, movie_ids, self_movie = recommend(selected_movie)
+    st.markdown('''
+            <center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=60%; height=auto;/>
+            </a>
+            <h3></h3>
+            <h4 style="font-family:verdana;">Top Recommendations for {}</h4>
+            </center><h2></h2>'''.format(self_movie[0], fetch_poster(self_movie[0]), self_movie[1]),
+            unsafe_allow_html=True
+        )
+    recommended_movie_names, recommended_movie_posters, movie_ids, self_movie = recommend(selected_movie)
+
+    col1, col2, col3 = st.columns(3)
     with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[0], movie_ids[0], recommended_movie_posters[0]),
+            unsafe_allow_html=True
+        )
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[3], movie_ids[3], recommended_movie_posters[3]),
+            unsafe_allow_html=True
+        )
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[6], movie_ids[6], recommended_movie_posters[6]),
+            unsafe_allow_html=True
+        )
     with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[1], movie_ids[1], recommended_movie_posters[1]),
+            unsafe_allow_html=True
+        )
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[4], movie_ids[4], recommended_movie_posters[4]),
+            unsafe_allow_html=True
+        )
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[7], movie_ids[7], recommended_movie_posters[7]),
+            unsafe_allow_html=True
+        )
 
     with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-# if __name__ == '__main__':
-#     main()
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[2], movie_ids[2], recommended_movie_posters[2]),
+            unsafe_allow_html=True
+        )
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[5], movie_ids[5], recommended_movie_posters[5]),
+            unsafe_allow_html=True
+        )
+        st.markdown('''
+            <center>
+            <p><b>{}</b></p>
+            </center>
+            <a style="text-decoration: none" href="https://www.themoviedb.org/movie/{}">
+            <img src={} width=100%; height=auto;/>
+            </a>
+            <h1></h1>
+            '''.format(recommended_movie_names[8], movie_ids[8], recommended_movie_posters[8]),
+            unsafe_allow_html=True
+        )
 
 
